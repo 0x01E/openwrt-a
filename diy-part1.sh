30f0c26a38f7c0eba33649181647d5a36f0b6fbc
0x01E <133861063+0x01E@users.noreply.github.com>
2026-04-10 14:34:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,4 +4,4 @@
 # Description: 更新 Feeds 前执行的脚本
 
 # 将 TurboACC 提取自大雕仓库的兼容分支持加入编译 feed (完美兼容官方版 openwrt)
-echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git' >> feeds.conf.default
+#echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git' >> feeds.conf.default
